681aefbea7607f6fb37d372f28213a4ec4e24a31
leticiafrancielle <leticiafrancielle2005@gmail.com>
2023-06-10 18:44:16 -0300

    subindo alterações

--- src/application/Menu.java
+++ src/application/Menu.java
@@ -12,39 +12,31 @@ import java.util.Scanner;
 public class Menu {
 
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
-		List<Livro> livros = new ArrayList<>();
-<<<<<<< HEAD
-
-=======
 		int opcao = 1;
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
+
+		List<Livro> livros = new ArrayList<>();
+
 		livros.add(new Livro("Dom quixote", 1));
 		livros.add(new Livro("Diario de um banana", 2));
 		livros.add(new Livro("O alquimista", 3));
 		Biblioteca b1 = new Biblioteca(livros);
 
 		System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "BEM VINDO À BIBLIOTECA FUTURISTA!" + Cores.TEXT_RESET);
-<<<<<<< HEAD
 
 		System.out.print("Informe seu usuário: ");
-=======
-		System.out.print(Cores.TEXT_WHITE_BOLD_BRIGHT + "Informe seu usuário: ");
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 		String usuario = sc.nextLine();
-		System.out.print("Informe sua senha: " + Cores.TEXT_RESET);
+		System.out.print("Informe sua senha: ");
 		int senha = sc.nextInt();
 		Bibliotecario user = new Bibliotecario(usuario, senha);
 
 		while (user.verificarSenha(usuario, senha) == false) {
-			System.out.print(Cores.TEXT_WHITE_BOLD_BRIGHT + "Informe seu usuário: ");
+			System.out.print("Informe seu usuário: ");
 			sc.nextLine();
 			usuario = sc.nextLine();
-			System.out.print("Informe sua senha: " + Cores.TEXT_RESET);
+			System.out.print("Informe sua senha: ");
 			senha = sc.nextInt();
 		}
-<<<<<<< HEAD
 
 		while (true) {
 			System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "\n*****************************************"
@@ -53,16 +45,6 @@ public class Menu {
 					+ "\n* 4 - Retirar um livro pelo código      *" + "\n* 5 - Devolver um livro                 *"
 					+ "\n* 6 - Encontrar um livro pelo código    *" + "\n* 7 - Listar livros disponíveis         *"
 					+ "\n* 8 - Sair                              *" + "\n*****************************************"
-=======
-		while (true) {
-			System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT 
-					+ "\n*****************************************"	+ "\n* Menu:                                 *" 
-					+ "\n* 1 - Cadastrar alune                   *"	+ "\n* 2 - Visualizar cadastros dos alunes   *"
-					+ "\n* 3 - Remover alune                     *"	+ "\n* 4 - Adicionar livro                   *"
-					+ "\n* 5 - Retirar um livro pelo código      *"	+ "\n* 6 - Devolver um livro                 *"
-					+ "\n* 7 - Encontrar um livro pelo código    *" + "\n* 8 - Listar livros disponíveis         *"
-					+ "\n* 9 - Sair                              *" + "\n*****************************************"
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 					+ Cores.TEXT_RESET);
 			System.out.print("\nEntre com a opção desejada: ");
 			try {
@@ -75,11 +57,7 @@ public class Menu {
 
 			switch (opcao) {
 			case 1:
-<<<<<<< HEAD
 				System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "Cadastrar alune \n" + Cores.TEXT_RESET);
-=======
-				System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "Cadastrar alune: \n" + Cores.TEXT_RESET);
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 				sc.nextLine();
 				System.out.print("Informe o nome do alune: ");
 				String nome = sc.nextLine();
@@ -88,85 +66,52 @@ public class Menu {
 				break;
 
 			case 2:
-<<<<<<< HEAD
 				System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "Visualizar cadastro alunes \n" + Cores.TEXT_RESET);
-=======
-				System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "Visualizar cadastro alunes: \n" + Cores.TEXT_RESET);
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 				user.listarCadastros();
 				user.keyPress();
 				break;
 
 			case 3:
-				System.out.println(Cores.TEXT_BLUE_BOLD_BRIGHT + "Remover alune: \n" + Cores.TEXT_RESET);
-				System.out.print("Entre com a matricula do alune que deseja retirar: ");
-				int matricula = sc.nextInt();
-				user.removerAluno(matricula);
-				user.keyPress();
-				break;
-			case 4:
 				System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "Adicionar livro \n" + Cores.TEXT_RESET);
 				sc.nextLine();
 				System.out.print("Informe o nome do livro: ");
 				String nomeLivro = sc.nextLine();
+
 				b1.adicionarLivro(nomeLivro);
 				user.keyPress();
 				break;
-<<<<<<< HEAD
 
 			case 4:
 				System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "Retirar livro \n" + Cores.TEXT_RESET);
-=======
-			case 5:
-				System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "Retirar livro: \n" + Cores.TEXT_RESET);
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 				System.out.print("Entre com o código do livro que deseja retirar: ");
 				int cod = sc.nextInt();
 				b1.retirarLivro(cod);
 				user.keyPress();
 				break;
-<<<<<<< HEAD
 				
 			case 5:
 				System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "Devolver livro \n" + Cores.TEXT_RESET);
-=======
-			case 6:
-				System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "Devolver livro: \n" + Cores.TEXT_RESET);
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 				System.out.print("Entre com o código do livro que deseja devolver: ");
 				cod = sc.nextInt();
 				b1.devolverLivro(cod);
 				user.keyPress();
 				break;
-<<<<<<< HEAD
 				
 			case 6:
 				System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "Encontrar livro \n" + Cores.TEXT_RESET);
-=======
-			case 7:
-				System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "Encontrar livro: \n" + Cores.TEXT_RESET);
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 				System.out.print("Informe o código do livro: ");
 				cod = sc.nextInt();
 				b1.encontrarLivro(cod);
 				user.keyPress();
 				break;
-<<<<<<< HEAD
 				
 			case 7:
-=======
-			case 8:
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 				System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "Listar livros: \n" + Cores.TEXT_RESET);
 				b1.listarLivros();
 				user.keyPress();
 				break;
-<<<<<<< HEAD
 				
 			case 8:
-=======
-			case 9:
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 				encerrar();
 				sc.close();
 				System.exit(0);
@@ -176,7 +121,8 @@ public class Menu {
 				break;
 			}
 			b1.aguardar();
-		}		
+		}
+
 	}
 
 	public static void encerrar() {
@@ -190,8 +136,5 @@ public class Menu {
 		}
 		System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "\nPrograma encerrado!" + Cores.TEXT_RESET);
 	}
-<<<<<<< HEAD
 
-=======
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 }
--- src/livraria/controller/Bibliotecario.java
+++ src/livraria/controller/Bibliotecario.java
@@ -41,22 +41,14 @@ public class Bibliotecario implements Cadastro {
 
 	@Override
 	public boolean verificarSenha(String Bibliotecario, int senha) {
-<<<<<<< HEAD
 		if (Bibliotecario.equals("teste") && senha == 123) {
-=======
-		if(Bibliotecario.equals("Admin") && senha == 123) {
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 			aguardarSenha();
-			System.out.println(Cores.TEXT_GREEN_BOLD_BRIGHT + "Login feito com sucesso!" + Cores.TEXT_RESET);
+			System.out.println(Cores.TEXT_GREEN_BOLD_BRIGHT + "\nLogin sucedido!\n" + Cores.TEXT_RESET);
 			keyPress();
 			return true;
 		} else {
-<<<<<<< HEAD
 			System.out.println(
 					Cores.TEXT_RED_BOLD_BRIGHT + "\nLogin incorreto, verique o usuário ou senha!\n" + Cores.TEXT_RESET);
-=======
-			System.out.println(Cores.TEXT_RED_BOLD_BRIGHT + "Login incorreto, verifique usuário/senha e tente novamente.\n" + Cores.TEXT_RESET);
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 			return false;
 		}
 	}
@@ -65,28 +57,17 @@ public class Bibliotecario implements Cadastro {
 	public void cadastrarAluno(String nome) {
 		Aluno aluno = new Aluno(nome, gerarNumero());
 		alunos.add(aluno);
-<<<<<<< HEAD
 		System.out
 				.println(Cores.TEXT_GREEN_BOLD_BRIGHT + "Alune " + aluno.getNome() + " cadastrado!" + Cores.TEXT_RESET);
-=======
-		System.out.println(Cores.TEXT_GREEN_BOLD_BRIGHT + "Alune " + aluno.getNome() + " cadastrado!" + Cores.TEXT_RESET);
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 		System.out.println(Cores.TEXT_GREEN_BOLD_BRIGHT + "Matrícula: " + aluno.getMatricula());
 	}
 
 	@Override
 	public void listarCadastros() {
-<<<<<<< HEAD
 		if (alunos.isEmpty()) {
 			System.out.println(Cores.TEXT_RED_BOLD_BRIGHT + "Nenhum alune cadastrado!" + Cores.TEXT_RESET);
 		}
 		for (Aluno alunos : alunos) {
-=======
-		if(alunos.isEmpty()) {
-			System.out.println(Cores.TEXT_RED_BOLD_BRIGHT + "Nenhum alune cadastrado!" + Cores.TEXT_RESET);
-		}
-		for(Aluno alunos : alunos) {
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 			System.out.println("° Alune: " + alunos.getNome() + ", matrícula: " + alunos.getMatricula());
 		}
 	}
@@ -102,16 +83,11 @@ public class Bibliotecario implements Cadastro {
 		Optional<Aluno> testaAluno = Optional.ofNullable(alunoRemovido);
 		if (testaAluno.isPresent()) {
 			alunos.remove(alunoRemovido);
-<<<<<<< HEAD
 			System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "O alune " + alunoRemovido.getNome() + " de matrícula: "
 					+ alunoRemovido.getMatricula() + " foi removido!" + Cores.TEXT_RESET);
 		} else {
 			System.out.println(Cores.TEXT_RED_BOLD_BRIGHT + "Não foi encontrado nenhum alune com esta matrícula: "
 					+ matricula + Cores.TEXT_RESET);
-=======
-			System.out.println(Cores.TEXT_BLUE_BOLD_BRIGHT +"O alune " + alunoRemovido.getNome()
-				+ " de matrícula: " + alunoRemovido.getMatricula() + " foi removido!" + Cores.TEXT_RESET);
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 		}
 	}
 
@@ -120,20 +96,14 @@ public class Bibliotecario implements Cadastro {
 			System.out.println(Cores.TEXT_RESET + "Pressione enter para continuar...");
 			int key = System.in.read();
 			if (key != 13) {
-<<<<<<< HEAD
 				System.out.println("Você pressionou uma tecla diferente de Enter!");
 			}
-=======
-	            System.out.println("Você pressionou uma tecla diferente de Enter!");
-	        }
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 		} catch (IOException e) {
 			System.out.println("Erro de entrada/saída ocorreu ao aguardar a tecla Enter!");
 		}
 	}
 
 	public static void aguardarSenha() {
-<<<<<<< HEAD
 		System.out.println("Verificando login...");
 		try {
 			Thread.sleep(1500);
@@ -143,17 +113,6 @@ public class Bibliotecario implements Cadastro {
 			return;
 		}
 	}
-=======
-        System.out.println("Verificando login...");
-        try {
-            Thread.sleep(1500); 
-        } catch (InterruptedException e) {
-        	Thread.currentThread().interrupt(); 
-            System.err.println("Erro ao verificar login!");
-            return;
-        }
-    }
->>>>>>> 813617deeb956a474ad46afc6e80e164f5d430e1
 
 	@Override
 	public int gerarNumero() {
